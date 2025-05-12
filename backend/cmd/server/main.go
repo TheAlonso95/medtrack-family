39b7d9eaa3fa675f0d635c6535187240e49b5400
Rafael Alonso Medeiros <rafael.a.m95@hotmail.com>
2025-05-12 23:26:19 +0000

    fix: issues

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -43,7 +43,8 @@ func main() {
 		<-sig
 
 		// Shutdown signal with grace period of 30 seconds
-		shutdownCtx, _ := context.WithTimeout(serverCtx, 30*time.Second)
+		shutdownCtx, shutdownCancel := context.WithTimeout(serverCtx, 30*time.Second)
+		defer shutdownCancel()
 
 		go func() {
 			<-shutdownCtx.Done()
